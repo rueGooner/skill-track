697331af1c2f18d9dffa419b71d4c729e2402715
Rueben <Rueben.cumberbatch@gamma.co.uk>
2025-05-01 22:24:52 +0100

    feat: install swagger docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const config = new DocumentBuilder()
+  .setTitle('SkillTrack API')
+  .setDescription('API documentation for SkillTrack')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 5555);
 }
 bootstrap();
