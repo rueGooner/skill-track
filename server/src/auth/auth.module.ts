9112c8dde637ce21a724e2679b2eb8646f6ad0cb
Rueben <Rueben.cumberbatch@gamma.co.uk>
2025-05-04 23:43:57 +0100

    feat: add register endpoint logic

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaService } from 'src/prisma.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService, JwtService],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      })
+    })
+  ]
 })
 export class AuthModule {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,15 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { PrismaService } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prisma: PrismaService, private jwtService: JwtService) {}
   create(registerPayload: RegisterDto) {
     return 'This action adds a new auth';
   }
 
-  register(registerPayload: RegisterDto) {
-    return 'This action registers a new user';
+  async register(registerPayload: RegisterDto) { 
+    const { email, password } = registerPayload;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
+
+    if (existingUser) throw new ConflictException('Email is already registered.');
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...registerPayload,
+        password: hashedPassword,
+      }
+    })
+    
+    const payload = { sub: newUser.id, email: newUser.email, role: newUser.role };
+    const access_token = this.jwtService.sign(payload);
+
+    return { access_token };
   }
 
 
